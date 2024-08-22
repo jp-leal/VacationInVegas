a650d5c50023a1a6102394fdec624ed70a925b6c
joaobug <joaoenfrance@gmail.com>
2024-08-22 14:47:14 -0300

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Joao Leal on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
